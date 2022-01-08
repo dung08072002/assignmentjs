a1480838f1fec173aca8ddcff7232927b1378abb
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-01-08 12:49:17 +0700

    set up navigo +  router

--- src/main.js
+++ src/main.js
@@ -1,1 +1,52 @@
-console.log("abc");
+import Navigo from "navigo";
+
+const router = new Navigo("/");
+
+// router.on("/", function(){
+//     console.log('Home page');
+// });
+// router.on("about", function(){
+//     console.log('About page');
+// });
+
+router.on({
+    "/": () => {
+        console.log('Home page');
+    },
+    "/about": () => {
+        console.log('About page');
+    }
+})
+
+router.resolve();
+
+// //ES5
+// function Animal(color){
+//     this.color = color;
+//     this.showInfor = function (){
+//         console.log(this.color);
+//     }
+// }
+// //Khởi tạo đối tượng
+// const cat = new Animal ("mau vang");
+// cat.showInfor();
+
+// //ES6
+// class ConVat{
+//     constructor(color){
+//         this.color = color;
+//     }
+//     showInfor() {
+//         console.log(this.color);
+//     }
+// }
+// const cat2 = new ConVat ("mau do");
+
+// //OBJECT LITERAL
+// const person = {
+//     name: 'Dung',
+//     showInfo(){
+//         console.log(this.name);
+//     }
+// };
+
