dcf8cd1d379762032ff8df5e3afea2e9f82852b7
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-01-14 03:57:00 +0700

    Create new page and data

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ const Footer = {
     print(){
         return `
             <footer class="max-w-6xl mx-auto bg-[#272f54] py-5 text-center">
-                <span class="text-white">NGÔ VIỆT DŨNG</span>
+                <span class="text-white">COPYRIGHT BY NGÔ VIỆT DŨNG</span>
             </footer>
         `;
     }
--- src/data.js
+++ src/data.js
@@ -1,39 +1,39 @@
 const data = [
     {
         id: 1,
-        title: "Tiêu đề bài viết 1",
+        title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ spring 2018",
         img: "https://picsum.photos/400/400",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias voluptatibus esse odit iusto adipisci id",
+        desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt động",
     },
     {
         id: 2,
-        title: "Tiêu đề bài viết 2",
+        title: "Sinh viên khối ngành kinh tế học cách chạm vào cảm xúc",
         img: "https://picsum.photos/400/400",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias voluptatibus esse odit iusto adipisci id",
+        desc: "Với sự tham gia của chuyên gia lĩnh vực kinh doanh, chương trình đã thu hút đồng bào sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi.",
     },
     {
         id: 3,
-        title: "Tiêu đề bài viết 3",
+        title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury",
         img: "https://picsum.photos/400/400",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias voluptatibus esse odit iusto adipisci id",
+        desc: "Với triết lý đào tạo \"Thực học thực nghiệp\" Cao đẳng FPolytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp",
     },
     {
         id: 4,
-        title: "Tiêu đề bài viết 4",
+        title: "Vinh danh 295 sinh viên FPT Polytechnic học kỳ spring 2018",
         img: "https://picsum.photos/400/400",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias voluptatibus esse odit iusto adipisci id",
+        desc: "Tối ngày 16/06/2018, Cao đẳng FPT Polytechnic Hà Nội đã tổ chức Lễ tôn vinh Ong vàng Poly, tuyên dương 295 cá nhân và tập thể có thành tích xuất sắc trong học tập cũng như hoạt động",
     },
     {
         id: 5,
-        title: "Tiêu đề bài viết 5",
+        title: "Sinh viên khối ngành kinh tế học cách chạm vào cảm xúc",
         img: "https://picsum.photos/400/400",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias voluptatibus esse odit iusto adipisci id",
+        desc: "Với sự tham gia của chuyên gia lĩnh vực kinh doanh, chương trình đã thu hút đồng bào sinh viên Cao đẳng FPT Polytechnic tham dự. Đặc biệt các bạn sinh viên bị lôi cuốn bởi.",
     },
     {
         id: 6,
-        title: "Tiêu đề bài viết 6",
+        title: "Sinh viên FPoly trải nghiệm thực tế tại Mường Thanh Luxury",
         img: "https://picsum.photos/400/400",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias voluptatibus esse odit iusto adipisci id",
+        desc: "Với triết lý đào tạo \"Thực học thực nghiệp\" Cao đẳng FPolytechnic Đà Nẵng thường xuyên tổ chức cho sinh viên tham quan các đơn vị doanh nghiệp",
     },
 ];
 
