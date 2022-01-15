3b9e8f49c9aa7d8d11d26cec54e881851583e8c3
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-01-15 12:19:48 +0700

    news manage import data

--- src/pages/admin/news/index.js
+++ src/pages/admin/news/index.js
@@ -56,48 +56,37 @@ const AdminNewsPage = {
             <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               image
             </th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-              description
-            </th>
-            <th scope="col" class="relative px-6 py-3">
+            <th scope="col" colspan="2"class="relative px-6 py-3">
               <span class="sr-only">Edit</span>
             </th>
           </tr>
         </thead>
         <tbody class="bg-white divide-y divide-gray-200">
-          <tr>
+            ${data.map((post) =>`
+            <tr>
             <td class="px-6 py-4 whitespace-nowrap">
               <div class="flex items-center">
-                <div class="flex-shrink-0 h-10 w-10">
-                  <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
-                </div>
-                <div class="ml-4">
+                <div class="">
                   <div class="text-sm font-medium text-gray-900">
-                    Jane Cooper
-                  </div>
-                  <div class="text-sm text-gray-500">
-                    jane.cooper@example.com
+                    ${post.id}
                   </div>
                 </div>
               </div>
             </td>
             <td class="px-6 py-4 whitespace-nowrap">
-              <div class="text-sm text-gray-900">Regional Paradigm Technician</div>
-              <div class="text-sm text-gray-500">Optimization</div>
+              <div class="text-sm text-gray-900">${post.title}</div>
             </td>
             <td class="px-6 py-4 whitespace-nowrap">
-              <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                Active
-              </span>
-            </td>
-            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              Admin
+                <img class="h-20 w-20" src="${post.img}" alt="">
             </td>
             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-              <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+            <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
             </td>
-          </tr>
-
+            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+              <a href="#" class="text-red-600 hover:text-indigo-900">Delete</a>
+            </td>
+            </tr>
+            `).join("")}
           <!-- More people... -->
         </tbody>
       </table>
@@ -111,7 +100,6 @@ const AdminNewsPage = {
             </div>
             </main>
         </div>
-        
         `;
     },
 };
