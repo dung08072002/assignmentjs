5a7291140475e51321f7624b9a1956543fdee2a6
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-01-23 12:32:20 +0700

    create api function product

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -1,35 +1,24 @@
 // import data from "../data";
+import axios from 'axios';
 
 const NewsList = {
-    render() {
-        return fetch("https://61e7a8b7e32cd90017acbbf2.mockapi.io/posts")
-            .then((response) => response.json())
-            .then((data) =>/* html */`
-                <div class="grid grid-cols-3 gap-5 mt-5">
-                    ${data.map((post) => `
-                            <div class="detail-content p-5 border-2 hover:">
-                                    <div class="img-content">
-                                        <a href="/news/${post.id}">
-                                            <img class="w-max" src="${post.img}" alt="" />
-                                        </a>
-                                    </div>
-                                    <div>
-                                    <a href="/news/${post.id}">
-                                        <h3 class="title-content text-2xl text-[#9d430f] font-semibold">
-                                            ${post.title}
-                                        </h3>
-                                    </a>
-                                    <p class="mt-3">
-                                        ${post.desc}
-                                    </p>
-                                </div>
-                        </div>
-                        <!-- end detail content -->
-                    `).join("")}
-                    
-                </div>
-        
-        `);
-    },
+    async render() {
+        // Chờ thằng axios.get truy cập API và lấy dữ liệu, 
+        // lấy dữ liệu xong sẽ trả về và gán vào biến response
+        const { data } = await axios.get("https://61e7a8b7e32cd90017acbbf2.mockapi.io/posts");
+        return /* html */`
+            <h2 class="font-semibold text-2xl uppercase text-blue-800 my-4">Tin tức học tập</h2>
+            <div class="grid grid-cols-3 gap-8">
+                ${data.map((post) => `
+                    <div class="border p-4">
+                        <a href="/news/${post.id}">
+                            <img src="${post.img}" alt="" />
+                        </a>
+                        <h3 class="my-3"><a  href="/news/${post.id}" class="font-semibold text-lg text-orange-500">${post.title}</a></h3>                    
+                        <p>${post.desc}</p>
+                    </div>
+                `).join("")}
+            </div>
+        `},
 };
 export default NewsList;
--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -37,9 +37,9 @@ const AdminNewsAddPage = {
             <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 <!-- Replace with your content -->
                 <div class="px-4 py-6 sm:px-0">
-                <div
-                    class="border-4 border-dashed border-gray-200 rounded-lg h-96"
-                ></div>
+                <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
+
+                </div>
                 </div>
                 <!-- /End replace -->
             </div>
@@ -47,5 +47,8 @@ const AdminNewsAddPage = {
         </div>
         `;
     },
+    afterRender(){
+           
+    }
 };
 export default AdminNewsAddPage;
