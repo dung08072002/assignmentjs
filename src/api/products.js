c69b31d9d1f01e15e27f53127b99a22bec5d383e
dung08072002 <dungnvph17931@fpt.edu.vn>
2022-02-15 13:31:04 +0700

    update api

--- src/api/products.js
+++ src/api/products.js
@@ -1,18 +1,18 @@
 import instance from "./config";
 
-export const add = (post) => {
-    const url = "/posts";
-    return instance.post(url, post);
+export const add = (product) => {
+    const url = "/products";
+    return instance.post(url, product);
 };
 export const get = (id) => {
-    const url = `/posts/${id}`;
+    const url = `/products/${id}`;
     return instance.get(url);
 };
 export const getAll = () => {
-    const url = "/posts";
+    const url = "/products";
     return instance.get(url);
 };
 export const remove = (id) => {
-    const url = `/posts/${id}`;
+    const url = `/products/${id}`;
     return instance.delete(url);
 };
